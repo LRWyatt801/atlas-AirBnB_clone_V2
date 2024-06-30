9665b8000898d7c910610e771265184343999aa2
Logan Wyatt <loganwyatt1995@gmail.com>
2024-06-30 17:38:01 +0000

    upload setup for sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+-- Prepare a server called "hbnb_dev_db" for the project
+
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
+-- Privileges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
