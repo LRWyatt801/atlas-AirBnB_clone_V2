28ec42bb7048f441a82bbd98207a41a680f13265
LRWyatt801 <loganwyatt1995@gmail.com>
2024-07-01 17:33:00 -0500

    Starting db_storage

---
+++ models/engine/db_storage.py
@@ -0,0 +1,26 @@
+"""Database storage module"""
+
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, create_engine
+
+
+class DBStorage:
+    """Class for database storage"""
+    
+    __engine = None
+    __session = None
+    
+    def __init__(self) -> None:
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(HBNB_MYSQL_USER,
+                                              HBNB_MYSQL_PWD,
+                                              HBNB_MYSQL_HOST,
+                                              HBNB_MYSQL_DB),
+                                      pool_pre_ping=True)
+        
