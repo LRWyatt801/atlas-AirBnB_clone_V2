620958c3a81159cd31ef57653fb6d6519558c6bf
LRWyatt801 <loganwyatt1995@gmail.com>
2024-07-15 19:59:09 -0500

    Add is_number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Starts a Flask web application on different routes"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """prints "Hello HBNB!"
+
+    Returns:
+        string: printed script
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Prints HBNB on route /hbnb
+
+    Returns:
+        str: return string
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello_world_in_c(text: str) -> str:
+    """adds the /c/<text> route
+
+    Args:
+        text (str): text to return
+
+    Returns:
+        str: sanitized text
+    """
+    sanitized_text = escape(text).replace('_', ' ')
+    return f"C {sanitized_text}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text: str = "is cool") -> str:
+    """adds route /python/<text>
+
+    Args:
+        text (str, optional): text to display. Defaults to "is cool".
+
+    Returns:
+        str: returns str of input text
+    """
+    sanitized_text = escape(text).replace('_', ' ')
+    return f"Python {sanitized_text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_number(n):
+    if isinstance(n, int):
+        return "{:d} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
